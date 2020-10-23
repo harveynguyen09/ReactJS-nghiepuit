726915e673270ef30919c69dc3c89431ae7c2f59
phuc hung <nguyenphuchung09@gmail.com>
2020-10-23 15:14:21 +0700

    session1

---
+++ src/demo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Demo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello ReactJS</h1>
+        <p>Chào mừng đến với khóa học lập trình reactJS</p>
+      </div>
+    );
+  }
+}
+
+export default Demo;
