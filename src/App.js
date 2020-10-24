b232c7151eee8477b6f183be011ff8cd68d7e487
phuc hung <nguyenphuchung09@gmail.com>
2020-10-24 17:22:19 +0700

    jsx

--- src/App.js
+++ src/App.js
@@ -5,7 +5,39 @@ import Header from './components/Header'
 import Product from './components/Product'
 
 class App extends Component {
+  showProductInfo(product) {
+    if (product.status) {
+      return (
+        <div>
+          <p>ID: {product.id}</p>
+          <p>Name: {product.name}</p>
+          <p>Quantity: {product.quantity}</p>
+        </div>
+      )
+
+    }
+  }
   render() {
+    let product = {
+      id: 1,
+      name: "fanta",
+      quantity: 100,
+      status: false
+    }
+    let passenger = [
+      { id: 1, name: 'abc', age: 32 },
+      { id: 2, name: 'bcd', age: 40 },
+      { id: 3, name: 'def', age: 50 }
+    ]
+    let renderPassenger = passenger.map((passenger, index) => {
+      return (
+        <div key={passenger.id}>
+          <p>ID: {passenger.id}</p>
+          <p>NAME: {passenger.name}</p>
+          <p>AGE: {passenger.age}</p>
+        </div>
+      )
+    })
     return (
       <div>
         <h1>App Component</h1>
@@ -21,7 +53,8 @@ class App extends Component {
             </div>
           </div>
         </div>
-
+        {this.showProductInfo(product)}
+        {renderPassenger}
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
+import '../App.css'
 
 
-class Header extends Component{
-  render () {
-    return (
-      
-      <nav className="navbar navbar-inverse">
-          <a className="navbar-brand" href="#">Title</a>
-          <ul className="nav navbar-nav">
-              <li className="active">
-                  <a href="#">Home</a>
-              </li>
-              <li>
-                  <a href="#">Link</a>
-              </li>
-          </ul>
-      </nav>
-      
-    );
-  }
+class Header extends Component {
+    render() {
+        var a = 5;
+        var b = 10;
+        var user = {
+            id: 123,
+            name: 'hung',
+            height: '1m70'
+        }
+        return (
+            <div>
+                <nav className="navbar navbar-inverse">
+                    <a className="navbar-brand" href="#">Title</a>
+                    <ul className="nav navbar-nav">
+                        <li className="active">
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Link</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='ml-30'>
+                    <div>{a}</div>
+                    <div>{a + b}</div>
+                    <ul>
+                        <li>ID: {user.id}</li>
+                        <li>Name: {user.name}</li>
+                        <li>Height: {user.height}</li>
+                    </ul>
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default Header;
